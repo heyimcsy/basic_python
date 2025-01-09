78f9b2428f9c5bc54ccd78e9f8c9f25d4cd0b30d
sychoi <cinnamon1995@naver.com>
2025-01-09 16:29:41 +0900

    function for

--- conditions.py
+++ conditions.py
@@ -8,3 +8,25 @@ else:
     print('걸어가자!')
 
 print('일단 집에 가자')
+
+fruits = ['apple', 'banana', 'kiwi', 'strawberry', 'melon']
+
+for fruit in fruits:
+    print(fruit)
+
+people = [
+    {'name': 'bob', 'age': 20},
+    {'name': 'carry', 'age': 38},
+    {'name': 'john', 'age': 7},
+    {'name': 'smith', 'age': 17},
+    {'name': 'ben', 'age': 27},
+    {'name': 'bobby', 'age': 57},
+    {'name': 'red', 'age': 32},
+    {'name': 'queen', 'age': 25}
+]
+
+for person in people:
+    name = person['name']
+    age = person['age']
+    if age >= 25:
+        print(name,'s age is', age)

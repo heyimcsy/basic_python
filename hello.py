11850c1ca0015cb5ee82677720d21544073a1a16
sychoi <cinnamon1995@naver.com>
2025-01-09 16:16:38 +0900

    function dict

--- hello.py
+++ hello.py
@@ -45,3 +45,18 @@ b_dict['height'] = 155
 result_b = b_dict
 
 print(result_b)
+
+# people = [{'name' : 'jon', 'age': 20}, {'name': 'jisoo', 'age': 17}]
+#
+# people.append(b_dict)
+# print(people)
+
+people = [
+    {'name': 'bob', 'age': 20, 'score':{'math':90,'science':70}},
+    {'name': 'carry', 'age': 38, 'score':{'math':40,'science':72}},
+    {'name': 'smith', 'age': 28, 'score':{'math':80,'science':90}},
+    {'name': 'john', 'age': 34, 'score':{'math':75,'science':100}}
+]
+
+# smith의 과학 점수
+print(people[2]['score']['science'])

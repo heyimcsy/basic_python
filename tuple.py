5d2644c5bfeda31f71918027adc045fa1a04f457
sychoi <cinnamon1995@naver.com>
2025-01-11 23:38:14 +0900

    function tyy catch

--- tuple.py
+++ tuple.py
@@ -42,3 +42,23 @@ for s in scores:
     score = s['score']
     print(name + '의 점수는 ' + str(score) + '점입니다.')
     print(f'{name}의 점수는 {score}점입니다.')
+
+people = [
+    {'name': 'bob', 'age': 20},
+    {'name': 'carry', 'age': 38},
+    {'name': 'john', 'age': 7},
+    {'name': 'smith', 'age': 17},
+    {'name': 'ben', 'age': 27},
+    {'name': 'bobby'},
+    {'name': 'red', 'age': 32},
+    {'name': 'queen', 'age': 35}
+]
+
+# bobby 가 age가 없으면 ?
+for person in people:
+    try:
+        if person['age'] > 20:
+            print(person['name'])
+    except:
+        print(person['name'], '에러입니다')
+

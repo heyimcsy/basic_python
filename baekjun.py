4701af4b1bb0461cfb1307207dfaef2e4d8606d4
sychoi <cinnamon1995@naver.com>
2025-01-31 22:28:09 +0900

    function 매개변수

--- baekjun.py
+++ baekjun.py
@@ -1,67 +1,78 @@
 import sys
+from collections import Counter
+
 from io import StringIO
 
 # 테스트 입력을 문자열로 제공
-test_input ="""3
-BANANA 2
-PLUM 4
-BANANA 3"""
+test_input ="""0 1 0 1
+1 1 1 0
+0 0 1 1"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
 # 코드 실행
-
 text = sys.stdin.read().splitlines()
 print(text)
-fruits = {'BANANA': 0, 'PLUM': 0, 'LIME': 0, 'STRAWBERRY': 0}
-for i in range(int(text[0])):
-    fruit, count = text[i + 1].split(' ')
-    fruit = str(fruit)
-    count = int(count)
-    if fruit in fruits:
-        fruits[fruit] = fruits[fruit] + count
-        print(fruits)
-if 5 in fruits.values():
-    print('YES')
-else:
-    print('NO')
 
-# test_list = [[10]*5 for i in range(5)]
-# print(test_list)
+for n in text:
+    print(n.count('0'))
+    if n.count('0') == 1:
+        print('A')
+    elif n.count('0') == 2:
+        print('B')
+    elif n.count('0') == 3:
+        print('C')
+    elif n.count('0') == 4:
+        print('D')
+    else:
+        print('E')
+# N, M = map(int, text[0].split(' '))
+# students = []
 #
-# for i in range(5):
-#     a = list(input())
-#     a_len = len(a)
-#     for j in range(a_len):
-#         test_list[i][j] = a[j]
-#
-# for i in range(10):
-#     for j in range(5):
-#         if test_list[j][i] == 10:
-#             continue
-#         else:
-#             print(test_list[j][i],end='')
-
-# 각 줄에서 조건 검사
-# for t in text:
-#     print(t[::1])
-    # if not any(sub in t for sub in substrings):
-    #     reversedText = ''.join(reversed(t))
-    #     print(reversedText)
+# for i in range(N):
+#     number = int(text[(i * 2)+1])
+#     studentNumbers = tuple(text[(i * 2) + 2].split(' '))
+#     students.extend(studentNumbers)
+# count = Counter(students)
+# print(count)
+# counts = sum(1 for value in count.values() if value >= M)
+# print(counts)
 
 
-# import sys
-# from io import StringIO
+
+
+
+
+
+
+
+# count, food, used = text
+# count = int(count)
+# food = list(food.split(' '))
+# used = list(used.split(' '))
 #
-# # 테스트 입력을 문자열로 제공
-# test_input = """Hello, World!
-# This is a test."""
+# used_dict = {item: True for item in used}
+# for item in food:
+#     if item not in used_dict:
+#         print('2222',item)
+
+
+
+# fruits = {'BANANA': 0, 'PLUM': 0, 'LIME': 0, 'STRAWBERRY': 0}
+# for i in range(int(text[0])):
+#     fruit, count = text[i + 1].split(' ')
+#     fruit = str(fruit)
+#     count = int(count)
+#     if fruit in fruits:
+#         fruits[fruit] = fruits[fruit] + count
+#         print(fruits)
+# if 5 in fruits.values():
+#     print('YES')
+# else:
+#     print('NO')
+
 #
-# # sys.stdin을 대체
-# sys.stdin = StringIO(test_input)
-#
-# # 코드 실행
-# input_text = sys.stdin.read()
-# print("입력받은 내용:")
-# print(input_text)
+
+# print(ord('b') - 96)
+

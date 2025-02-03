1f9608b0e5e1916ada178a610bb930012b58de52
sychoi <cinnamon1995@naver.com>
2025-02-03 21:11:06 +0900

    function 매개변수

--- baekjun.py
+++ baekjun.py
@@ -1,6 +1,4 @@
 import sys
-from collections import Counter
-
 from io import StringIO
 
 # 테스트 입력을 문자열로 제공
@@ -27,52 +25,3 @@ for n in text:
         print('D')
     else:
         print('E')
-# N, M = map(int, text[0].split(' '))
-# students = []
-#
-# for i in range(N):
-#     number = int(text[(i * 2)+1])
-#     studentNumbers = tuple(text[(i * 2) + 2].split(' '))
-#     students.extend(studentNumbers)
-# count = Counter(students)
-# print(count)
-# counts = sum(1 for value in count.values() if value >= M)
-# print(counts)
-
-
-
-
-
-
-
-
-
-# count, food, used = text
-# count = int(count)
-# food = list(food.split(' '))
-# used = list(used.split(' '))
-#
-# used_dict = {item: True for item in used}
-# for item in food:
-#     if item not in used_dict:
-#         print('2222',item)
-
-
-
-# fruits = {'BANANA': 0, 'PLUM': 0, 'LIME': 0, 'STRAWBERRY': 0}
-# for i in range(int(text[0])):
-#     fruit, count = text[i + 1].split(' ')
-#     fruit = str(fruit)
-#     count = int(count)
-#     if fruit in fruits:
-#         fruits[fruit] = fruits[fruit] + count
-#         print(fruits)
-# if 5 in fruits.values():
-#     print('YES')
-# else:
-#     print('NO')
-
-#
-
-# print(ord('b') - 96)
-

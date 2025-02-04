2c4d6ad82f0363fd8c1cb7b3225d109143827127
sychoi <cinnamon1995@naver.com>
2025-02-05 00:25:10 +0900

    function 매개변수

--- baekjun.py
+++ baekjun.py
@@ -2,25 +2,48 @@ import sys
 from io import StringIO
 
 # 테스트 입력을 문자열로 제공
-test_input ="""5
-5
+test_input ="""6
+6
+9
+7
+6
 4
-3
-2
-1"""
+6"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-# 코드 실행
-text = sys.stdin.read().splitlines()
-print(text)
+# # 코드 실행
+# text = sys.stdin.read().splitlines()
+# print(text)
+#
+# tall = 0
+# count = 0
+# for number in text[:0:-1]:
+#     number = int(number)
+#     if number > tall:
+#         tall = number
+#         count += 1
+# print(count)
 
-tall = 0
+text = sys.stdin.read().splitlines()
+
+# heights = list(map(int, text[1:]))
+# stack = [heights[-1]]
+# # 뒤에서부터 탐색
+# for height in reversed(heights):
+#     if stack[-1] < height:
+#         print('wile',stack)
+#         stack.append(height)
+# print(len(stack))
+
+heights = list(map(int, text[1:]))
+stack = []
 count = 0
-for number in text[:0:-1]:
-    number = int(number)
-    if number > tall:
-        tall = number
+
+for height in reversed(heights):
+    if not stack or stack[-1] < height:
+        stack.append(height)
         count += 1
+
 print(count)

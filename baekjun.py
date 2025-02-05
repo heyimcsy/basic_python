4238f77e361a15471837234cd92511d35ad707bd
sychoi <cinnamon1995@naver.com>
2025-02-06 01:11:55 +0900

    function 매개변수

--- baekjun.py
+++ baekjun.py
@@ -1,49 +1,44 @@
 import sys
+from collections import deque
 from io import StringIO
 
 # 테스트 입력을 문자열로 제공
-test_input ="""6
-6
-9
-7
-6
-4
-6"""
+test_input ="""15
+push 1
+push 2
+front
+back
+size
+empty
+pop
+pop
+pop
+size
+empty
+pop
+push 3
+empty
+front"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-# # 코드 실행
-# text = sys.stdin.read().splitlines()
-# print(text)
-#
-# tall = 0
-# count = 0
-# for number in text[:0:-1]:
-#     number = int(number)
-#     if number > tall:
-#         tall = number
-#         count += 1
-# print(count)
-
 text = sys.stdin.read().splitlines()
+queue = deque()
+N = int(text[0])
+lists = text[1:]
 
-# heights = list(map(int, text[1:]))
-# stack = [heights[-1]]
-# # 뒤에서부터 탐색
-# for height in reversed(heights):
-#     if stack[-1] < height:
-#         print('wile',stack)
-#         stack.append(height)
-# print(len(stack))
-
-heights = list(map(int, text[1:]))
-stack = []
-count = 0
-
-for height in reversed(heights):
-    if not stack or stack[-1] < height:
-        stack.append(height)
-        count += 1
-
-print(count)
+for command in lists:
+    split_command = command.split()
+    if split_command[0] == 'push':
+        queue.append(int(split_command[1]))
+    elif split_command[0] == 'pop':
+        print(queue.popleft() if queue else -1)
+    elif split_command[0] == 'size':
+        print(len(queue))
+    elif split_command[0] == 'empty':
+        print(1 if not queue else 0)
+    elif split_command[0] == 'front':
+        print(queue[0] if queue else -1)
+    elif split_command[0] == 'back':
+        print(queue[-1] if queue else -1)

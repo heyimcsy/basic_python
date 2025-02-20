9690acaa2fe2428c8685a384e5526bbf01df57b1
sychoi <cinnamon1995@naver.com>
2025-02-21 02:21:31 +0900

    백준 9498

--- baekjun.py
+++ baekjun.py
@@ -1,15 +1,22 @@
 import sys
 from io import StringIO
 
-import calendar as cd
-
-# 백준 2753
-test_input ="""2401"""
+# 백준 9498
+test_input ="""94"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-Y = int(sys.stdin.readline())
+S = int(sys.stdin.readline())
 
+if S >= 90:
+    print("A")
+elif S >= 80:
+    print("B")
+elif S >= 70:
+    print("C")
+elif S >= 60:
+    print("D")
+else:
+    print("F")
 
-print(1 if cd.isleap(Y) else 0)

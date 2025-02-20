c4b5cddddd21eeac74ef4639a41a9988374a4cf8
sychoi <cinnamon1995@naver.com>
2025-02-21 01:47:57 +0900

    백준 2884

--- baekjun.py
+++ baekjun.py
@@ -1,11 +1,21 @@
 import sys
 from io import StringIO
 
-# 백준 11382
-test_input ="""77 77 7777"""
+# 백준 2884
+test_input ="""23 40"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-A, B, C = map(int, sys.stdin.readline().split(' '))
-print(A+B+C)
+H, M = map(int, sys.stdin.readline().split(' '))
+
+if M - 45 >= 0:
+    M = M - 45
+else:
+    if H == 0:
+        H = 23
+    else:
+        H = H - 1
+    M = 60 + (M - 45)
+time = [H, M]
+print(" ".join(map(str, time)))

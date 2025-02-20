dcb4f39c26dcae8d7f1448cd33530ca81640e419
sychoi <cinnamon1995@naver.com>
2025-02-21 02:59:29 +0900

    백준 1009

--- baekjun.py
+++ baekjun.py
@@ -1,13 +1,23 @@
 import sys
 from io import StringIO
 
-# 백준 2739
-test_input ="""2"""
+# 백준 1009
+test_input ="""5
+1 6
+3 7
+6 2
+7 100
+9 635"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
 N = int(sys.stdin.readline())
-for i in range(9):
-    result = N * (i + 1)
-    print(f'{N} * {i + 1} = {result}')
+
+for i in range(N):
+    F, S = list(map(int,sys.stdin.readline().split(" ")))
+    if F % 10 == 0:
+        print(10)
+    else:
+        last_digit = pow(F, S, 10)
+        print(last_digit)

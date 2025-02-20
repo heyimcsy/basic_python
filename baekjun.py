2b083e69ce5d2a59b555367b14e001bd1908e48e
sychoi <cinnamon1995@naver.com>
2025-02-21 02:35:44 +0900

    백준 2739

--- baekjun.py
+++ baekjun.py
@@ -1,22 +1,13 @@
 import sys
 from io import StringIO
 
-# 백준 9498
-test_input ="""94"""
+# 백준 2739
+test_input ="""2"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-S = int(sys.stdin.readline())
-
-if S >= 90:
-    print("A")
-elif S >= 80:
-    print("B")
-elif S >= 70:
-    print("C")
-elif S >= 60:
-    print("D")
-else:
-    print("F")
-
+N = int(sys.stdin.readline())
+for i in range(9):
+    result = N * (i + 1)
+    print(f'{N} * {i + 1} = {result}')

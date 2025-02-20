f9cdfb154e2c16097b6b63879f5b0f6f5a6e4920
sychoi <cinnamon1995@naver.com>
2025-02-20 17:00:19 +0900

    function 매개변수

--- baekjun.py
+++ baekjun.py
@@ -2,32 +2,29 @@ import sys
 from io import StringIO
 
 # 테스트 입력을 문자열로 제공
-test_input ="""2
-
-1 1
-1
-1
-
-3 2
-1 3 2
-5 5"""
+test_input ="""3 4
+JLA
+CRUO
+3"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
+_ = sys.stdin.readline().strip()
+first = list(reversed(sys.stdin.readline().strip()))  # 첫 번째 그룹은 뒤집기
+second = list(sys.stdin.readline().strip())  # 두 번째 그룹은 그대로
 N = int(sys.stdin.readline().strip())
-text = [line.strip() for line in sys.stdin.readlines() if line.strip()]  # 공백 줄 제거
-for i in range(N):
-    sejin = sorted(list(map(int, text[(i * 3) + 1].split(' '))), reverse=True)
-    sebi = sorted(list(map(int, text[(i * 3) + 2].split(' '))), reverse=True)
 
-    # 최댓값 찾기
-    max_sejin = sejin[0]
-    max_sebi = sebi[0]
+direction = {ant: 0 for ant in first}  # 첫 번째 그룹: 오른쪽(0)
+direction.update({ant: 1 for ant in second})  # 두 번째 그룹: 왼쪽(1)
 
-    if sejin[0] >= sebi[0]:
-        print("S")
-    elif sebi[0] > sejin[0]:
-        print("B")
-    else:
-        print("C")
+text = first + second
+
+for _ in range(N):
+    i = 0
+    while i < len(text) - 1:
+        if direction[text[i]] == -0 and direction[text[i + 1]] == 1:
+            text[i], text[i + 1] = text[i+1], text[i]
+            i += 1
+        i += 1
+print("".join(text))

28b0f1b439f50defaa68c7b969d6e05049abb971
sychoi <cinnamon1995@naver.com>
2025-02-21 02:16:38 +0900

    백준 2753

--- baekjun.py
+++ baekjun.py
@@ -1,6 +1,8 @@
 import sys
 from io import StringIO
 
+import calendar as cd
+
 # 백준 2753
 test_input ="""2401"""
 
@@ -9,11 +11,5 @@ sys.stdin = StringIO(test_input)
 
 Y = int(sys.stdin.readline())
 
-if Y % 100 != 0 and Y % 4 == 0:
-    print(1)
-elif Y % 100 == 0 and Y % 400 == 0:
-    print(1)
-elif Y % 100 == 0 and Y % 400 != 0:
-    print(0)
-else:
-    print(0)
+
+print(1 if cd.isleap(Y) else 0)

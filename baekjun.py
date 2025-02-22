136e04fe5bdf691bc538988cd5d33b89a0daab9e
sychoi <cinnamon1995@naver.com>
2025-02-22 13:49:37 +0900

    백준 10773

--- baekjun.py
+++ baekjun.py
@@ -3,34 +3,33 @@ import heapq
 import sys
 from io import StringIO
 
-# 백준 28278
-test_input ="""9
-4
-1 3
-1 5
+# 백준 10773
+test_input ="""10
+1
 3
-2
 5
-2
-2
-5"""
+4
+0
+0
+7
+0
+0
+6"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-N = int(sys.stdin.readline().strip())
-stack = []
-for i in range(N):
-    line = sys.stdin.readline().strip()
+input = sys.stdin.read
+data = input().splitlines()
 
-    if "1" in line:
-        number = int(line.split(" ")[1])
-        stack.append(number)
-    elif line == "2":
-        print(stack.pop() if len(stack) else -1)
-    elif line == "3":
-        print(len(stack))
-    elif line == "4":
-        print(0 if len(stack) else 1)
+N = int(data[0])  # 첫 번째 줄에서 N 값 가져오기
+stack = []
+
+for i in range(1, N + 1):
+    number = int(data[i])
+    if number == 0:
+        stack.pop()
     else:
-        print(stack[-1] if len(stack) else -1)
+        stack.append(number)
+
+print(sum(stack))

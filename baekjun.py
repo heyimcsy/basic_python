5ddbb42349649a106aedbcea0f54488a0a3a2d87
sychoi <cinnamon1995@naver.com>
2025-02-24 01:09:11 +0900

    백준 10813

--- baekjun.py
+++ baekjun.py
@@ -3,20 +3,19 @@ from io import StringIO
 import sys
 import heapq
 
-# 백준 10810
+# 백준 10813
 test_input ="""5 4
-1 2 3
-3 4 4
-1 4 1
-2 2 2"""
+1 2
+3 4
+1 4
+2 2"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 N, M = map(int, sys.stdin.readline().split())
-baskets = {i: 0 for i in range(1, N + 1)}
 
+baskets = {i: i for i in range(1, N + 1)}
 for _ in range(M):
-    i, j, k = map(int, sys.stdin.readline().strip().split())
-    for n in range(i, j+1):
-        baskets[n] = k
+    i, j = map(int, sys.stdin.readline().split())
+    baskets[i], baskets[j] = baskets[j], baskets[i]
 print(*baskets.values())

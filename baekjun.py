fc2a84cb8e3c9d76cb2913bab5e142546730c17c
sychoi <cinnamon1995@naver.com>
2025-02-24 16:17:17 +0900

    백준 9012

--- baekjun.py
+++ baekjun.py
@@ -1,16 +1,35 @@
 from io import StringIO
 import sys
 
-# 백준 11718
-test_input = """Hello
-Baekjoon
-Online Judge"""
+# 백준 9012
+test_input = """6
+(())())
+(((()())()
+(()())((()))
+((()()(()))(((())))()
+()()()()(()()())()
+(()((())()("""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
-def strip(x):
-    return x.strip()
-S = list(map(strip, sys.stdin.readlines()))
 
-for i in range(len(S)):
-    print(S[i])
+N = int(sys.stdin.readline())
+
+for _ in range(N):
+    stack = []
+    text = sys.stdin.readline().strip()
+    is_valid = True
+    for c in text:
+        if c == '(':
+            stack.append(c)
+        else:
+            if not stack:
+                is_valid = False
+                break
+            stack.pop()
+
+    # 남아 있는 '('가 있으면 잘못된 괄호 문자열
+    if is_valid and not stack:
+        print("YES")
+    else:
+        print("NO")

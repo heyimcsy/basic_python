1492036dca45144e08e638365fd78b6124391670
sychoi <cinnamon1995@naver.com>
2025-02-24 14:47:46 +0900

    백준 2908

--- baekjun.py
+++ baekjun.py
@@ -1,12 +1,18 @@
 from io import StringIO
 import sys
 
-# 백준 1152
-test_input = """The Curious Case of Benjamin Button"""
+# 백준 2908
+test_input = """734 893"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-S = sys.stdin.readline().split()
 
-print(len(S))
+def swap_digits(x):
+    num_list = list(str(x))
+    num_list[0], num_list[2] = num_list[2], num_list[0]
+    return int("".join(num_list))
+
+numbers = list(map(swap_digits, sys.stdin.readline().split()))
+
+print(max(numbers))

d02184728fa7d63dec087abf541b3c6f4830bda5
sychoi <cinnamon1995@naver.com>
2025-02-24 14:21:29 +0900

    백준 11720

--- baekjun.py
+++ baekjun.py
@@ -1,12 +1,16 @@
 from io import StringIO
 import sys
 
-# 백준 11654
-test_input = """A"""
+# 백준 11720
+test_input = """11
+10987654321"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-N = sys.stdin.readline()
-
-print(ord(N))
+N = int(sys.stdin.readline())
+number = sys.stdin.readline().strip()
+sum_number = 0
+for i in range(N):
+    sum_number += int(number[i])
+print(sum_number)

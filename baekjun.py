27c57d8d618e0521c5682d1888bebb3ed2ad1a26
sychoi <cinnamon1995@naver.com>
2025-02-24 12:16:11 +0900

    백준 10811

--- baekjun.py
+++ baekjun.py
@@ -1,27 +1,26 @@
+import math
 from io import StringIO
 
 import sys
 import heapq
 
-# 백준 3052
-test_input = """39
-40
-41
-42
-43
-44
-82
-83
-84
-85"""
+# 백준 10811
+test_input = """5 4
+1 2
+3 4
+1 4
+2 2"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
-# N, M = map(int, sys.stdin.readline().split())
+N, M = map(int, sys.stdin.readline().split())
 
-def sep(x):
-    return int(x) % 42
+baskets = {i: i for i in range(1, N+1)}
 
-number = list(map(sep, sys.stdin.readlines()))
+for _ in range(M):
+    f, s = map(int, sys.stdin.readline().split())
+    count = math.ceil((s -f) /2)
+    for i in range(count):
+        baskets[f + i] , baskets[s - i] = baskets[s - i] , baskets[f + i]
 
-print(len(set(number)))
+print(*baskets.values())

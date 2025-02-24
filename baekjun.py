9086d8593e7f1dc962de4b32c90c4bc593bdc6d0
sychoi <cinnamon1995@naver.com>
2025-02-24 14:34:30 +0900

    백준 1152

--- baekjun.py
+++ baekjun.py
@@ -1,22 +1,12 @@
 from io import StringIO
 import sys
 
-# 백준 10809
-test_input = """baekjoon"""
+# 백준 1152
+test_input = """The Curious Case of Benjamin Button"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-S = sys.stdin.readline()
+S = sys.stdin.readline().split()
 
-alpha = {'a': -1, 'b': -1, 'c': -1, 'd': -1, 'e': -1,
-         'f': -1, 'g': -1, 'h': -1, 'i': -1, 'j': -1,
-         'k': -1, 'l': -1, 'm': -1, 'n': -1, 'o': -1,
-         'p': -1, 'q': -1, 'r': -1, 's': -1, 't': -1,
-         'u': -1, 'v': -1, 'w': -1, 'x': -1, 'y': -1, 'z': -1}
-for i in range(len(S)):
-    char = S[i]
-    if alpha[char] == -1:
-        alpha[char] = i
-
-print(*alpha.values())
+print(len(S))

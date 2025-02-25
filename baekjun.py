d981786ea1562ac772a4754a8ef66c1bc7aeb88d
sychoi <cinnamon1995@naver.com>
2025-02-25 16:08:08 +0900

    백준 2941

--- baekjun.py
+++ baekjun.py
@@ -1,19 +1,28 @@
 import math
 from io import StringIO
 import sys
+import re
 
-# 백준 10988
-test_input = """levvel"""
+# 백준 2941
+test_input = """dz=ak"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
 word = sys.stdin.readline().strip()
 
-check = True
-half = math.floor(len(word) / 2)
-for i in range(half):
-    if word[i] != word[- (i + 1)]:
-        check = False
-        break
-print(1 if check else 0)
+qro = ['c=', 'c-', 'd-', 'lj', 'nj', 's=', 'z=']
+
+first = ['c', 'd', 'l', 'n', 's', 'z']
+w = 0
+sum = 0
+while w < len(word):
+       if word[w : w + 3] == 'dz=' :
+              w += 3
+       elif word[w : w + 2] in qro:
+              w+= 2
+       else:
+              w+=1
+       sum += 1
+
+print(sum)

3c8d36e99ba5ba32cb7d60e07968aea4851189f5
sychoi <cinnamon1995@naver.com>
2025-02-27 02:05:48 +0900

    백준 2292

--- baekjun.py
+++ baekjun.py
@@ -2,15 +2,17 @@ import math
 from io import StringIO
 import sys
 
-# 백준 2903
-test_input = """5"""
+# 백준 2292
+test_input = """54"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
 N = int(sys.stdin.readline())
 
-point = 2
-for _ in range(N):
-    point = (point * 2) - 1
-print(point*point)
+count = 1
+cnt = 1
+while N > count:
+    count += 6 * cnt
+    cnt += 1
+print(cnt)

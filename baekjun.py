f49092a497bd3c363b1bea5f135ae50edeb49aa7
sychoi <cinnamon1995@naver.com>
2025-02-27 00:54:02 +0900

    백준 2566

--- baekjun.py
+++ baekjun.py
@@ -2,28 +2,30 @@ import math
 from io import StringIO
 import sys
 
-# 백준 2738
-test_input = """3 3
-1 1 1
-2 2 2
-0 1 0
-3 3 3
-4 4 4
-5 5 100"""
+# 백준 2566
+test_input = """3 23 85 34 17 74 25 52 65
+10 7 39 42 88 52 14 72 63
+87 42 18 78 53 45 18 84 53
+34 28 64 85 12 16 75 36 55
+21 77 45 35 28 75 90 76 1
+25 87 65 15 28 11 37 28 74
+65 27 75 41 7 89 78 64 39
+47 47 70 45 23 65 3 41 44
+87 13 82 38 31 12 29 29 80"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
+matrix = []
+for _ in range(9):
+    line = list(map(int, sys.stdin.readline().split()))
+    matrix.append(line)
 
-N, M = map(int, sys.stdin.readline().split())
-text = sys.stdin.readlines()
+number = max(map(max ,matrix))
 
-for i in range(N):
-    first_t = text[i].split()
-    second_t = text[i + N].split()
-    numbers = []
-    for j in range(M):
-        n_sum = int(first_t[j]) + int(second_t[j])
-        numbers.append(str(n_sum))
-
-    print(' '.join(numbers))
+print(number)
+for i in range(9):
+    for j in range(9):
+        if matrix[i][j] == number:
+            index = [str(i + 1), str(j + 1)]
+            print(' '.join(index))

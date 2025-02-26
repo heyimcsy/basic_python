1697dca89ed3f73dc47bf557d04cecef4f124987
sychoi <cinnamon1995@naver.com>
2025-02-27 01:41:23 +0900

    백준 2720

--- baekjun.py
+++ baekjun.py
@@ -2,19 +2,34 @@ import math
 from io import StringIO
 import sys
 
-# 백준 11005
-test_input = """60466175 36"""
+# 백준 2720
+test_input = """3
+124
+25
+194"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-N, B = map(int, sys.stdin.readline().split())
+N = int(sys.stdin.readline())
 
-def solution(n, b):
-    rev_base = ''
-    arr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    while n:
-        rev_base += str(arr[n % b])
-        n //= b
-    return rev_base[::-1]
-print(solution(N, B))
+for _ in range(N):
+    changes = {'Q': 0,'D': 0, 'N': 0, 'P':0 }
+    change = int(sys.stdin.readline().strip())
+    if change > 24:
+        count = math.floor(change / 25)
+        changes['Q'] = count
+        change -= 25 * count
+    if change > 9:
+        count = math.floor(change / 10)
+        changes['D'] = count
+        change -= 10 * count
+    if change > 4:
+        count = math.floor(change / 5)
+        changes['N'] = count
+        change -= 5 * count
+    if change < 5:
+        count = math.floor(change / 1)
+        changes['P'] = count
+
+    print(*changes.values())

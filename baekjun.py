1a93d269d8aab6956c27a9f66ba4f3f87126623b
sychoi <cinnamon1995@naver.com>
2025-02-26 18:23:23 +0900

    백준 1316

--- baekjun.py
+++ baekjun.py
@@ -2,14 +2,30 @@ import math
 from io import StringIO
 import sys
 
-# 백준 2475
-test_input = """0 4 2 5 6"""
+# 백준 1316
+test_input = """5
+ab
+aa
+aca
+ba
+bb"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-def cal(x):
-    return int(x) ** 2
+N = int(sys.stdin.readline())
+check = 0
+for _ in range(N):
+    word = sys.stdin.readline().strip()
+    alphabet_dict = {chr(i): 0 for i in range(97, 123)}
+    group_word = True
+    for i in range(len(word)):
+        if alphabet_dict[word[i]] == 0:
+            alphabet_dict[word[i]] = 1
+        elif alphabet_dict[word[i]] == 1 and word[i-1] != word[i]:
+            group_word = False
 
-M = list(map(cal, sys.stdin.readline().split()))
-print(sum(M) % 10)
+    if group_word:
+        check += 1
+print(check)
+

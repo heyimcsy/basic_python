f08fd8ba6c108f0c95965613001f28db61732224
sychoi <cinnamon1995@naver.com>
2025-02-27 00:05:57 +0900

    백준 2738

--- baekjun.py
+++ baekjun.py
@@ -2,40 +2,28 @@ import math
 from io import StringIO
 import sys
 
-# 백준 25206
-test_input = """ObjectOrientedProgramming1 3.0 A+
-IntroductiontoComputerEngineering 3.0 A+
-ObjectOrientedProgramming2 3.0 A0
-CreativeComputerEngineeringDesign 3.0 A+
-AssemblyLanguage 3.0 A+
-InternetProgramming 3.0 B0
-ApplicationProgramminginJava 3.0 A0
-SystemProgramming 3.0 B0
-OperatingSystem 3.0 B0
-WirelessCommunicationsandNetworking 3.0 C+
-LogicCircuits 3.0 B0
-DataStructure 4.0 A+
-MicroprocessorApplication 3.0 B+
-EmbeddedSoftware 3.0 C0
-ComputerSecurity 3.0 D+
-Database 3.0 C+
-Algorithm 3.0 B0
-CapstoneDesigninCSE 3.0 B+
-CompilerDesign 3.0 D0
-ProblemSolving 4.0 P"""
+# 백준 2738
+test_input = """3 3
+1 1 1
+2 2 2
+0 1 0
+3 3 3
+4 4 4
+5 5 100"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
 
-chihoon = 0
-total = 0
-score = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 'B0': 3.0, 'C+': 2.5, 'C0': 2.0, 'D+': 1.5, 'D0':1.0, 'F': 0.0}
-for _ in range(20):
-    _, D, S = sys.stdin.readline().split()
-    credit = float(D)
-    if S != 'P':
-        chihoon += score[S] * credit
-        total += credit
+N, M = map(int, sys.stdin.readline().split())
+text = sys.stdin.readlines()
 
-print(chihoon/total)
+for i in range(N):
+    first_t = text[i].split()
+    second_t = text[i + N].split()
+    numbers = []
+    for j in range(M):
+        n_sum = int(first_t[j]) + int(second_t[j])
+        numbers.append(str(n_sum))
+
+    print(' '.join(numbers))

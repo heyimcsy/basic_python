4b6d97b42c41250f0cabb0347aec77892129db20
sychoi <cinnamon1995@naver.com>
2025-02-26 18:43:55 +0900

    백준 25206

--- baekjun.py
+++ baekjun.py
@@ -2,30 +2,40 @@ import math
 from io import StringIO
 import sys
 
-# 백준 1316
-test_input = """5
-ab
-aa
-aca
-ba
-bb"""
+# 백준 25206
+test_input = """ObjectOrientedProgramming1 3.0 A+
+IntroductiontoComputerEngineering 3.0 A+
+ObjectOrientedProgramming2 3.0 A0
+CreativeComputerEngineeringDesign 3.0 A+
+AssemblyLanguage 3.0 A+
+InternetProgramming 3.0 B0
+ApplicationProgramminginJava 3.0 A0
+SystemProgramming 3.0 B0
+OperatingSystem 3.0 B0
+WirelessCommunicationsandNetworking 3.0 C+
+LogicCircuits 3.0 B0
+DataStructure 4.0 A+
+MicroprocessorApplication 3.0 B+
+EmbeddedSoftware 3.0 C0
+ComputerSecurity 3.0 D+
+Database 3.0 C+
+Algorithm 3.0 B0
+CapstoneDesigninCSE 3.0 B+
+CompilerDesign 3.0 D0
+ProblemSolving 4.0 P"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-N = int(sys.stdin.readline())
-check = 0
-for _ in range(N):
-    word = sys.stdin.readline().strip()
-    alphabet_dict = {chr(i): 0 for i in range(97, 123)}
-    group_word = True
-    for i in range(len(word)):
-        if alphabet_dict[word[i]] == 0:
-            alphabet_dict[word[i]] = 1
-        elif alphabet_dict[word[i]] == 1 and word[i-1] != word[i]:
-            group_word = False
 
-    if group_word:
-        check += 1
-print(check)
+chihoon = 0
+total = 0
+score = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 'B0': 3.0, 'C+': 2.5, 'C0': 2.0, 'D+': 1.5, 'D0':1.0, 'F': 0.0}
+for _ in range(20):
+    _, D, S = sys.stdin.readline().split()
+    credit = float(D)
+    if S != 'P':
+        chihoon += score[S] * credit
+        total += credit
 
+print(chihoon/total)

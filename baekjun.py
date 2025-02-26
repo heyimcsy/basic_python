3901e0c2c1fa1607e583a20a97bacd08d7e9a662
sychoi <cinnamon1995@naver.com>
2025-02-27 01:14:06 +0900

    백준 2745

--- baekjun.py
+++ baekjun.py
@@ -2,21 +2,12 @@ import math
 from io import StringIO
 import sys
 
-# 백준 2563
-test_input = """3
-3 7
-15 7
-5 2"""
+# 백준 2745
+test_input = """ZZZZZ 36"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-N = int(sys.stdin.readline())
-background = {}
-for _ in range(N):
-    width, height = map(int,sys.stdin.readline().split())
+N, B = sys.stdin.readline().split()
 
-    for w in range(width, width+10):
-        for h in range(height, height+10):
-            background[w,h] = 1
-print(len(background.values()))
+print(int(N, int(B)))

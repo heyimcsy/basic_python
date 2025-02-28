5019115ca24750ffa7f8dea4f12e14807d693c7d
sychoi <cinnamon1995@naver.com>
2025-03-01 01:07:14 +0900

    백준 2798

--- baekjun.py
+++ baekjun.py
@@ -1,22 +1,21 @@
 import math
 from io import StringIO
 import sys
-import heapq
+from itertools import combinations
 
-# 백준 1436 브루트 포스 알고리즘
-test_input = """500"""
+# 백준 2798
+test_input = """5 21
+5 6 7 8 9"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
-N = int(sys.stdin.readline().strip())
-count = 0
-number = 666
+N, M = map(int, sys.stdin.readline().split())
+cards = list(sorted(map(int, sys.stdin.readline().split())))
+print(cards)
+max_sum = 0
 
-while True:
-    if '666' in str(number):
-        count += 1
-    if count == N:
-        break
-    number += 1
-
-print(number)
+for combo in combinations(cards, 3):
+    current = sum(combo)
+    if current <= M:
+        max_sum = max(max_sum, current)
+print(max_sum)

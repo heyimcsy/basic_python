d2a11287797b51bd7f66d4e2fe78edb07f1cee7a
sychoi <cinnamon1995@naver.com>
2025-03-01 00:26:05 +0900

    백준 28702

--- baekjun.py
+++ baekjun.py
@@ -3,31 +3,31 @@ from io import StringIO
 import sys
 import heapq
 
-# 백준 1927
-test_input = """9
-0
-12345678
-1
-2
-0
-0
-0
-0
-32"""
+# 백준 28702
+test_input = """Fizz
+13
+14"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-N = int(sys.stdin.readline())
+first = sys.stdin.readline().strip()
+second = sys.stdin.readline().strip()
+third = sys.stdin.readline().strip()
 
-numbers = []
-for _ in range(N):
-    number = int(sys.stdin.readline())
-    if number == 0:
-        if len(numbers):
-            n = heapq.heappop(numbers)
-            print(n)
-        else:
-            print(0)
-    else:
-        heapq.heappush(numbers, number)
+number = 0
+if first.isdigit():
+    number = int(first) + 3
+elif second.isdigit():
+    number = int(second) + 2
+elif third.isdigit():
+    number = int(third) + 1
+
+if number % 3 == 0 and number % 5 == 0:
+    print('FizzBuzz')
+elif number % 3 == 0:
+    print('Fizz')
+elif number % 5 == 0:
+    print('Buzz')
+else:
+    print(number)

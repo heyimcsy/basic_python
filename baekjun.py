7241144647dfbbe5b92eb2465bb2a8d17def2c35
sychoi <cinnamon1995@naver.com>
2025-02-28 17:10:32 +0900

    백준 5086

--- baekjun.py
+++ baekjun.py
@@ -2,15 +2,21 @@ import math
 from io import StringIO
 import sys
 
-# 백준 2869
-test_input = """2 1 5"""
+# 백준 5086
+test_input = """8 16
+32 4
+17 5
+0 0"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
-A, B, V = map(int,sys.stdin.readline().split())
-
-if (V - B) % (A - B) == 0:
-    print((V - A) // (A - B) + 1)
-else:
-    print((V - A) // (A - B) + 2)
+text = sys.stdin.readlines()
+for i in range(len(text)-1):
+    A, B = map(int,text[i].split())
+    if A > B and A % B == 0:
+        print('multiple')
+    elif B > A and B % A == 0:
+        print('factor')
+    else:
+        print('neither')

af1fb314ce99332522375782f031383ab70c9d62
sychoi <cinnamon1995@naver.com>
2025-02-28 19:01:44 +0900

    백준 1927

--- baekjun.py
+++ baekjun.py
@@ -1,30 +1,33 @@
 import math
 from io import StringIO
 import sys
+import heapq
 
-# 백준 2581
-test_input = """60
-100"""
+# 백준 1927
+test_input = """9
+0
+12345678
+1
+2
+0
+0
+0
+0
+32"""
 
 # sys.stdin을 대체
 sys.stdin = StringIO(test_input)
 
 N = int(sys.stdin.readline())
-M = int(sys.stdin.readline())
-numbers = []
-for number in range(N, M + 1):
-    if number < 2:
-        continue
-    is_true = True
-    for n in range(2, number):
-        if number % n == 0:
-            is_true = False
-            break
-    if is_true:
-        numbers.append(number)
 
-if len(numbers) == 0:
-    print(-1)
-else:
-    print(sum(numbers))
-    print(numbers[0])
+numbers = []
+for _ in range(N):
+    number = int(sys.stdin.readline())
+    if number == 0:
+        if len(numbers):
+            n = heapq.heappop(numbers)
+            print(n)
+        else:
+            print(0)
+    else:
+        heapq.heappush(numbers, number)
